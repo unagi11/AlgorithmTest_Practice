709b7bcd4da2d3ea3afe60fc115a0a4818b26578
Unagi11 <unagi11@naver.com>
2022-03-06 21:45:22 +0900

    파일정리, cpp 환경설정

---
+++ cpp/leet_125/125.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPalindrome(string s)
+    {
+        string temp;
+
+        for (string::iterator it = s.begin(); it < s.end(); it++)
+        {
+            *it = tolower(*it);
+
+            if ((*it >= 'a' && *it <= 'z') || (*it >= '0' && *it <= '9'))
+                temp.append(1, *it);
+        }
+
+        for (int i = 0; i < temp.length(); i++)
+            if (temp[i] != temp[temp.length() - 1 - i])
+                return false;
+
+        return true;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution sol;
+
+    cout << sol.isPalindrome("hello") << endl;
+    cout << sol.isPalindrome("A man, a plan, a canal: Panama") << endl;
+    cout << sol.isPalindrome("race a car") << endl;
+
+    return 0;
+}
